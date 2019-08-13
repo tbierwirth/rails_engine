9ace87bc74904fcde370f74f96b7ad534e4ef1f8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-13 14:15:21 -0600

    Create test for merchant index and show

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(3)
+  end
+
+  it "can get one merchant my its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["id"].to_i).to eq(id)
+  end
+end
