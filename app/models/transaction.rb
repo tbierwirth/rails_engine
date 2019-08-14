12a5b0cd0c983e1c6ac6552e5c6b473ab25a3e19
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-14 16:35:37 -0600

    Add scope for successful transaction and transaction by customer method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,11 @@ class Transaction < ApplicationRecord
                         :result
 
   belongs_to :invoice
+
+  default_scope { order(id: :asc) }
+  scope :successful, -> { where(result: "success") }
+
+  def self.trans_by_customer(customer_id)
+    joins(:invoice).where("invoices.customer_id = #{customer_id}")
+  end
 end
