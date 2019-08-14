1f08d2393ddaf7e8f0233054d08a21e10dad8609
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-13 21:18:44 -0600

    Fix merchant find_by

--- app/controllers/api/v1/merchants_find_controller.rb
+++ app/controllers/api/v1/merchants_find_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::MerchantsFindController < ApplicationController
 
   def show
-    render json: MerchantSerializer.new(Merchant.find_by(name: params[:name]))
+    render json: MerchantSerializer.new(Merchant.find_by(find_params))
+  end
+
+  private
+
+  def find_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
 
 end
