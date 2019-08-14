0a4143b4b07fb693cdf0b42af0d9f76c54642cc3
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-13 21:40:40 -0600

    Add route for find_all

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'merchants/find/', to: 'merchants_find#show'
+      get 'merchants/find_all/', to: 'merchants_find#index'
       resources :merchants, only: [:index, :show]
     end
   end
