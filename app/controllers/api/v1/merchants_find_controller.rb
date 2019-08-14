89b8ad466c6dfd7c882302aa705ca6508123e310
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-13 21:40:48 -0600

    Add index action for find_all merchants

--- app/controllers/api/v1/merchants_find_controller.rb
+++ app/controllers/api/v1/merchants_find_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::MerchantsFindController < ApplicationController
 
+  def index
+    render json: MerchantSerializer.new(Merchant.where(find_params))
+  end
+
   def show
     render json: MerchantSerializer.new(Merchant.find_by(find_params))
   end
