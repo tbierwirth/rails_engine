60348aa7959de813388547ef45b8c6270cdb4515
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-18 08:03:18 -0600

    Add unit price conversion to serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,7 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :merchant_id, :description, :name, :unit_price
+  attributes :id, :merchant_id, :description, :name
+  attribute :unit_price do |object|
+    '%.2f' % (object.unit_price.to_f / 100)
+  end
 end
