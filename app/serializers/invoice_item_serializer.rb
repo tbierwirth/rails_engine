b7631db31abc67068c43100d7566553f83c187af
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-17 18:25:05 -0600

    Add customer serializer and additional attributes to invoice_items and transactions serializer

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,4 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id
+  attributes :id, :invoice_id, :item_id, :quantity, :unit_price
 end
--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,6 +1,6 @@
 class TransactionSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :invoice_id
+  attributes :id, :invoice_id, :credit_card_number, :result
 
   belongs_to :invoice
 end
